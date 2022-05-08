b7e6eb6993ca4322ae53debda115f6e6bd406b34
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-05-07 20:44:37 -0500

    creada ruta para explorers.json en app js

--- app/app.js
+++ app/app.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
 
 // Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json");
+const rawdata = fs.readFileSync("./../explorers.json");
 const explorers = JSON.parse(rawdata);
+
+console.log("Hey i'm here!")
+console.log(explorers)
 
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
