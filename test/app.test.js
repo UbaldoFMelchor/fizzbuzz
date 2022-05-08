cbb09a3d1df81c3373bfa49cb6ab9203a88074f1
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-05-07 22:11:20 -0500

    modificado test para mejor confiabilidad

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,18 @@
 const Reader = require("./../lib/utils/Reader")
 const ExplorerServices = require("./../lib/services/ExplorerServices")
 
-test('1.- Reader ok', () => {
-    const explorers = Reader.readJsonFile("explorers.json")
-      expect(explorers).toBe(explorers);
-  });
+describe("Test para Reader", () => {
+    test('1.- Reader ok', () => {
+        const explorers = Reader.readJsonFile("explorers.json")
+          expect(explorers).toBe(explorers);
+      });
+})
 
-test('2.- Explorer Services: filterbymission', () => {
-    const explorerByMission = ExplorerServices.filterByMission()
-        expect(explorerByMission).toBe(explorerByMission);
-
+describe("Test para ExplorerServices", () => {
+    test('1.- filterbymission', () => {
+        const explorers = [{mission: "node"}]
+        const explorersInNode = ExplorerServices.filterByMission(explorers, "node");
+            expect(explorersInNode.length).toBe(1);
+    
+    });
 })
