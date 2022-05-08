954048a8c05b74a59a36f81e4d723e22404380e1
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-05-08 03:02:00 -0500

    formateando con eslint

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -1,21 +1,21 @@
-const ExplorerController = require("./../../../lib/controllers/ExplorerController")
-const ExplorerServices = require("./../../../lib/services/ExplorerServices")
-const FizzbuzzServices = require("./../../../lib/services/Fizzbuzzservice")
-const Reader = require("./../../../lib/utils/Reader")
+const ExplorerController = require("./../../../lib/controllers/ExplorerController");
+const ExplorerServices = require("./../../../lib/services/ExplorerServices");
+const FizzbuzzServices = require("./../../../lib/services/Fizzbuzzservice");
+const Reader = require("./../../../lib/utils/Reader");
 
 describe("Test para ExplorerController", () => {
     test("1.- getExplorersByMission length", () => {
         const ExplorersByMissionLength = ExplorerController.getExplorersByMission("node");        
-        expect(ExplorersByMissionLength).toHaveLength(10)
-    })
+        expect(ExplorersByMissionLength).toHaveLength(10);
+    });
 
     test("2.- getExplorersUsernameByMission", () => {
         const ExplorersUsernameByMission = ExplorerController.getExplorersUsernameByMission("node");
-        expect(ExplorersUsernameByMission).toContain("ajolonauta15")
-    })
+        expect(ExplorersUsernameByMission).toContain("ajolonauta15");
+    });
 
     test("3.- getExplorersAmountByMission", () => {
         const ExplorersAmountByMission = ExplorerController.getExplorersAmountByMission("java");
-        expect(ExplorersAmountByMission).toBe(5)
-    })
-})
+        expect(ExplorersAmountByMission).toBe(5);
+    });
+});
