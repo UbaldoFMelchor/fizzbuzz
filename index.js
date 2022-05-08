9a9a191be879af5c2921046820655ed32d862fd0
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-05-07 21:55:46 -0500

    modificado el codigo para que test 2 de pass

--- index.js
+++
@@ -1,6 +0,0 @@
-const Reader = require("./lib/utils/Reader");
-const explorers = Reader.readJsonFile("explorers.json");
-
-ExplorerService.filterByMission(explorers, "node");
-ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-ExplorerService.getExplorersUsernamesByMission(explorers, "node");
--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,13 @@
 const Reader = require("./../lib/utils/Reader")
+const ExplorerServices = require("./../lib/services/ExplorerServices")
 
 test('1.- Reader ok', () => {
     const explorers = Reader.readJsonFile("explorers.json")
       expect(explorers).toBe(explorers);
   });
-  
+
+test('2.- Explorer Services: filterbymission', () => {
+    const explorerByMission = ExplorerServices.filterByMission()
+        expect(explorerByMission).toBe(explorerByMission);
+
+})
